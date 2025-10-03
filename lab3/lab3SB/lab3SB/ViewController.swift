a03792c59b09099dce8f90c1068036cd547b90e0
AnonimHakerrr <oleksandrsoltis@gmail.com>
2025-10-03 23:53:47 +0300

    gradient

--- lab3/lab3SB/lab3SB/ViewController.swift
+++ lab3/lab3SB/lab3SB/ViewController.swift
@@ -17,3 +17,60 @@ class ViewController: UIViewController {
 
 }
 
+@IBDesignable
+class GradientView: UIView {
+    
+    // MARK: - Inspectable properties
+    @IBInspectable var startColor: UIColor = .blue {
+        didSet { updateGradient() }
+    }
+    
+    @IBInspectable var endColor: UIColor = .cyan {
+        didSet { updateGradient() }
+    }
+
+    @IBInspectable var horizontal: Bool = false {
+        didSet { updateGradient() }
+    }
+
+    @IBInspectable var diagonal: Bool = false {
+        didSet { updateGradient() }
+    }
+
+    // MARK: - Override layer class
+    override class var layerClass: AnyClass {
+        return CAGradientLayer.self
+    }
+
+    private var gradientLayer: CAGradientLayer {
+        return self.layer as! CAGradientLayer
+    }
+
+    // MARK: - Update gradient
+    private func updateGradient() {
+        gradientLayer.colors = [startColor.cgColor, endColor.cgColor]
+        
+        var startPoint = CGPoint(x: 0, y: 0)
+        var endPoint = CGPoint(x: 0, y: 1)
+
+        if horizontal {
+            startPoint = CGPoint(x: 0, y: 0.5)
+            endPoint = CGPoint(x: 1, y: 0.5)
+        }
+
+        if diagonal {
+            startPoint = CGPoint(x: 0, y: 0)
+            endPoint = CGPoint(x: 1, y: 1)
+        }
+
+        gradientLayer.startPoint = startPoint
+        gradientLayer.endPoint = endPoint
+    }
+
+    // MARK: - Layout updates
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        updateGradient()
+    }
+}
+
